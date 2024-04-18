4453f15eb318e244cbec7df1e26da357c69a26c6
Angel Samuel Espinoza <759412@student.dusd.net>
2024-04-18 13:19:30 -0700

    just did the bounse
    just did the bounse

--- Unit3LabSoundAndEffectsEspinozaP5/Assets/Scripts/PlayerControl.cs
+++ Unit3LabSoundAndEffectsEspinozaP5/Assets/Scripts/PlayerControl.cs
@@ -12,12 +12,18 @@ public class PlayerControl : MonoBehaviour
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
     // Start is called before the first frame update
     void Start()
     {
      playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
      Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +35,16 @@ public class PlayerControl : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&& !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce,ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -46,6 +62,7 @@ public class PlayerControl : MonoBehaviour
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
--- Unit3LabSoundAndEffectsEspinozaP5/Assets/Scripts/SpawnManager.cs
+++ Unit3LabSoundAndEffectsEspinozaP5/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerControl playerControlScript;
+    private int randomObstacle;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,8 @@ public class SpawnManager : MonoBehaviour
     {
         if (playerControlScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
         }
     }
 }
